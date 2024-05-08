5947f2fafbce045aba1a0b7bcf50c5465bc5932e
Myrella Almeida <myrella.alvesbcn@outlook.com>
2024-05-08 13:58:43 +0200

    ejercicio 3

--- nivellUno/index.php
+++ nivellUno/index.php
@@ -6,6 +6,10 @@
     define("name","Myrella Almeida");
     $hello   = "Hello World!";
     $curso   = "Aquest és el curso de PHP";
+    $x       = 5;
+    $y       = 10;
+    $n       = 2.5;
+    $m       = 7.5;
 
     echo '<h3> Ejercicio 1 - Create Variables </h3>';
     echo 'La variable $number es de tipo: '.gettype($number)." y tiene valor: $number"."<br>"; 
@@ -18,5 +22,14 @@
     echo strtoupper($hello)."<br>";
     echo 'La variable $hello tiene '.strlen($hello)." de longitud"."<br>";
     echo strrev($hello)."<br>";
-    echo $hello." ".$curso;
+    echo $hello." ".$curso."<br>";
+    echo "<h3>"."Ejercicio 3 -  Operators PHP"."</h3>";
+    echo 'La variable $x tiene valor: '.$x.' y La variable $y tiene valor: '.$y."<br>";
+    echo 'La suma de $x + $y es igual a: '.$x+$y."<br>";
+    echo 'La resta de $x - $y es igual a: '.$x-$y."<br>";
+    echo 'El producto de $x * $y es igual a: '.$x*$y."<br>";
+    echo 'El modulo de $x por $y es igual a: '.$x%$y."<br>";
+    
+    
+
 ?>
